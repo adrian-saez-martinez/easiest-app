1e33996938c262f4fe301be8eb82410b648ebb14
adrian-saez-martinez <128140712+adrian-saez-martinez@users.noreply.github.com>
2024-10-23 14:15:49 +0200

    tst3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,4 @@
 import streamlit as st
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-# Load the tokenizer and model
-tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-1.5B-Instruct")
-model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-1.5B-Instruct")
 
 # Streamlit app layout
 st.title("🎈 My new app with Qwen model")
