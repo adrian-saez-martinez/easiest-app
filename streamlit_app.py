7eeac81780521dd3f6564854cef69b8a55beafef
adrian-saez-martinez <128140712+adrian-saez-martinez@users.noreply.github.com>
2024-10-23 14:20:45 +0200

    trying pipe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
+# Load the tokenizer and model
+messages = [
+    {"role": "user", "content": "Who are you?"},
+]
+pipe = pipeline("text-generation", model="Qwen/Qwen2.5-1.5B-Instruct")
+pipe(messages)
+
 # Streamlit app layout
 st.title("🎈 My new app with Qwen model")
 st.write("Let's start building a simple app with the Qwen model.")
